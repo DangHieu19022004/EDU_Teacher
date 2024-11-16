6bbdfe1f7b600d74bcbe1ba52652eda0bad262df
HieuDang <127599956+DangHieu19022004@users.noreply.github.com>
2024-11-17 00:14:25 +0700

    PTTK pattern

--- mobile_app/ORC_Bank/app/_layout.tsx
+++ mobile_app/ORC_Bank/app/_layout.tsx
@@ -1,5 +1,5 @@
 import { Slot, SplashScreen, Stack } from "expo-router";
-import { useFonts } from 'expo-font';
+import { useFonts } from "expo-font";
 
 // Import your global CSS file
 import "../global.css";
@@ -9,28 +9,21 @@ import { useEffect } from "react";
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    "Roboto-Mono": require("../assets/fonts/RobotoMono-Regular.ttf"),
+  });
 
-    const [fontsLoaded, error] = useFonts({
-        "Roboto-Mono" : require("../assets/fonts/RobotoMono-Regular.ttf"),
-    });
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
 
-    useEffect(() => {
-        if(error) throw error;
-        if(fontsLoaded) SplashScreen.hideAsync();
-    }, [fontsLoaded, error])
+  if (!fontsLoaded) return null;
+  if (!fontsLoaded && !error) return null;
 
-
-    if(!fontsLoaded) return null;
-    if(!fontsLoaded && !error) return null;
-
-    return (
-        <Stack>
-            
-            <Stack.Screen 
-                name="index"
-                options={{headerShown: false}}
-            />
-            
-        </Stack>
-    )
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
