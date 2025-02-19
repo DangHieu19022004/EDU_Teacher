a6a7e5c2dc7a0770eba35ff0e8d4f6ebedab2f15
KhacTrung <trungnk153@gmail.com>
2025-02-19 14:16:59 +0700

    clean

--- mobile_app/backend/User/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import home, logout_view
-
-urlpatterns = [
-    # path("api/auth/google/", google_login, name="google_login"),
-    path("", home),
-    path("logout", logout_view),
-]
--- mobile_app/backend/scan_orc/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import getDataReceipt
-
-urlpatterns = [
-    path('saveinfor/', getDataReceipt, name='getDataReceipt')
-
-]
-
